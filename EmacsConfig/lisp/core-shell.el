bf36c8f72045cd6bb321f2a26ea856a62a4b5768
Random Logic 5 <279862+randomlogic0101@users.noreply.github.com>
2026-03-19 08:18:20 -0400

    Changed core-shell.el to check /run/current-system/sw/bin/bash, and /run/current-system/sw/bin/bash, for the path to the shell, and then fall back to /usr/bin/bash to make the M-x shell command more robust for several platforms. In particular this works on both NixOS, MacOS, and Debians.

--- EmacsConfig/lisp/core-shell.el
+++ EmacsConfig/lisp/core-shell.el
@@ -1,12 +1,15 @@
 ;;; core-shell.el -*- lexical-binding: t; -*-
 
-(use-package shell
-  :ensure nil
+(use-package shell :ensure nil
   :init
-  (setq explicit-shell-file-name "/usr/bin/bash")
+  ;; Try to find a bash shell
+  (setq explicit-shell-file-name (or (getenv "BASH")
+				     (getenv "SHELL")
+				     "/usr/bin/bash"))
   (setenv "SHELL" explicit-shell-file-name)
   (setenv "TERM" "xterm-256color")
   :config
   (setq comint-terminfo-terminal "xterm-256color"))
 
 (provide 'core-shell)
+
